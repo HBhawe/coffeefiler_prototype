5cdd9810db695085b9e9b0a19eb3bd56bb26d1e7
Harshal Bhawe <harshalb97@outlook.com>
2025-08-25 20:39:47 +0200

    Changes to some components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <>
+      <NavBar username={username} isAuthenticated={isAuthenticated} />
       <AppName />
       {isAuthenticated ? (
         <AppLogin username={username} />
@@ -25,6 +26,29 @@ function App() {
         />
       )}
     </>
+  );
+}
+
+function NavBar({ username, isAuthenticated }) {
+  return (
+    <div className="navbar">
+      <div className="title">
+        <span>CoffeeFiler</span>
+      </div>
+      <div className="function-buttons">
+        <button className="navbar-toggler" type="button">
+          Register
+        </button>
+        <button className="navbar-toggler" type="button">
+          Login
+        </button>
+        {isAuthenticated ? (
+          <button className="navbar-toggler" type="button">
+            {username}'s profile
+          </button>
+        ) : null}
+      </div>
+    </div>
   );
 }
 
--- src/components/AppLogin.jsx
+++ src/components/AppLogin.jsx
@@ -2,6 +2,7 @@ export function AppLogin({ username }) {
   return (
     <div className="welcome">
       <h4>Welcome to CoffeeFiler {username}</h4>
+      <p>A coffee tracking project among friends</p>
     </div>
   );
 }
